c5d8d29130daa366222bc59eecd348dcbb920eaa
nischal shetty <nischalshetty85@gmail.com>
2011-04-03 17:00:36 +0530

    unwanted commit

---
+++ src/com/face4j/dummy/Dummy.java
@@ -0,0 +1,200 @@
+package com.face4j.dummy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.face4j.facebook.Client;
+import com.face4j.facebook.Facebook;
+import com.face4j.facebook.OAuthAccessToken;
+import com.face4j.facebook.criteria.ConnectionColumnCriteria;
+import com.face4j.facebook.entity.Comment;
+import com.face4j.facebook.entity.Data;
+import com.face4j.facebook.entity.Post;
+import com.face4j.facebook.entity.User;
+import com.face4j.facebook.entity.connection.Comments;
+import com.face4j.facebook.enums.ConnectionColumn;
+import com.face4j.facebook.enums.Display;
+import com.face4j.facebook.enums.Paging;
+import com.face4j.facebook.enums.Permission;
+import com.face4j.facebook.enums.TargetType;
+import com.face4j.facebook.exception.FacebookException;
+import com.face4j.facebook.factory.FacebookFactory;
+import com.face4j.facebook.fql.FqlPost;
+
+public class Dummy {
+
+	private static String clientId = "05c5dd19dde59bfaa8921bc0b30889c4";
+	private static String clientSecret = "f152bc483259c4403aa26dd9a75b7a8d";
+	//private static String code = null;
+	private static String code = "135280156521587|2c0ac3e6905b4265a3527140-100001125197404|CwIrliPnLbLLHnvoCrUQ1OxWo4Q"; //ninjachacha
+	private static String redirectUrl = "http://buffr.com/login_verify.html";
+
+	public static void main(String[] args) throws IOException, FacebookException {
+		//System.out.println(getAccessToken().getAccessToken());
+
+		OAuthAccessToken accessToken = new OAuthAccessToken(code);
+		Client client = new Client(clientId, clientSecret);
+		FacebookFactory facebookFactory = new FacebookFactory(client);
+		Facebook facebook = facebookFactory.getInstance(accessToken);
+		
+		List<ConnectionColumn> columnNames = new ArrayList<ConnectionColumn>();
+		columnNames.add(ConnectionColumn.IS_DELETED);
+		columnNames.add(ConnectionColumn.IS_FOLLOWING);
+		columnNames.add(ConnectionColumn.SOURCE_ID);
+		columnNames.add(ConnectionColumn.TARGET_ID);
+		columnNames.add(ConnectionColumn.TARGET_TYPE);
+		columnNames.add(ConnectionColumn.UPDATED_TIME);
+		
+		ConnectionColumnCriteria columnCriteria = new ConnectionColumnCriteria();
+		columnCriteria.setTargetType(TargetType.PAGE);
+		
+		/*FqlConnection[] connections = facebook.getConnection(columnNames, columnCriteria);
+		
+		for(FqlConnection connection : connections){
+			System.out.println(connection.getTargetId());
+			//User user = facebook.getUser(connection.getTargetId());
+		}
+		*/
+		
+		FqlPost[] fqlPost = facebook.newsFeed();
+		
+		for(FqlPost fqlPost2 : fqlPost){
+		
+		System.out.println("Post id: "+fqlPost2.getPostId()+" source id: "+fqlPost2.getSourceId());	
+		Post post = facebook.getPost(fqlPost2.getPostId());
+		
+		if(post!=null){
+			System.out.println(post.getId());
+			System.out.println(post.getName());
+			System.out.println(post.getPicture());
+			System.out.println(post.getCreatedTime());
+
+				// -----------Likes Test-----------
+				// Likes likes = post.getConnectionLikes(facebook);
+				//
+				// if (likes != null) {
+				// System.out.println("Hurray! Likes is not null.");
+				// for (Data data : likes.getData()) {
+				// System.out.println("ID: " + data.getId() + " Name: " + data.getName());
+				// }
+				//
+				// System.out.println("Done with the first set, getting next");
+				//
+				// likes = likes.getNext(facebook);
+				//
+				// if (likes != null) {
+				// System.out.println("Hurray! Likes next pagination works!");
+				// for (Data data : likes.getData()) {
+				// System.out.println("ID: " + data.getId() + " Name: " + data.getName());
+				// }
+				// }
+				//
+				// likes = likes.getPrevious(facebook);
+				//
+				// if (likes != null) {
+				// System.out.println("Hurray! Likes prev pagination works!");
+				// for (Data data : likes.getData()) {
+				// System.out.println("ID: " + data.getId() + " Name: " + data.getName());
+				// }
+				// }
+				// }
+				// -----------Likes Test-----------
+
+				// -----------Comments Test-----------
+
+				Map<Paging, String> pagingCriteria = new HashMap<Paging, String>();
+				pagingCriteria.put(Paging.LIMIT, "3");
+				Comments comments = post.getConnectionsComments(facebook, pagingCriteria);
+
+				if (comments != null) {
+					System.out.println("Hurray! Comments is not null.");
+					
+					String[] concatFbIds = new String[comments.getComments().size()];
+					int i=0;
+					for (Comment data : comments.getComments()) {
+						System.out.println("ID: " + data.getId() + " Name: " + data.getFrom().getName()
+								+ " Comment: "+data.getMessage());
+						
+						concatFbIds[i] = ""+data.getFrom().getId();
+						i++;
+					}
+
+					User[] users = facebook.getUsers(concatFbIds);
+					
+					System.out.println(users[0].getFirstName());
+					
+					System.out.println("Done with the first set, getting next");
+
+					comments = comments.getNext(facebook);
+
+					if (comments != null) {
+						System.out.println("Hurray! Comments next pagination works!");
+						for (Comment data : comments.getComments()) {
+							System.out.println("ID: " + data.getId() + " Name: " + data.getFrom().getName());
+						}
+
+					comments = comments.getPrevious(facebook);
+
+					if (comments != null) {
+						System.out.println("Hurray! Comments prev pagination works!");
+						for (Comment data : comments.getComments()) {
+							System.out.println("ID: " + data.getId() + " Name: " + data.getFrom().getName());
+						}
+					}
+				}
+					
+				}
+				// -----------Comments Test-----------
+
+			//break;
+		}
+		}
+		
+		// User fbUser = facebook.getCurrentUser();
+		// System.out.println(fbUser.getFirstName());
+
+		/*FqlPost[] posts = facebook.newsFeed();
+
+		if (posts != null) {
+			for (FqlPost post : posts) {
+
+				System.out.println("Message: " + post.getMessage());
+				if (post.getAttachment() != null) {
+					System.out.println("" + post.getAttachment().getHref());
+				}
+			}
+		}*/
+		
+		//facebook.postLink("http://justunfollow.com");
+	}
+
+	private static OAuthAccessToken getAccessToken() throws IOException, FacebookException {
+		Client client = new Client(clientId, clientSecret);
+		FacebookFactory facebookFactory = new FacebookFactory(client);
+		OAuthAccessToken accessToken = null;
+
+		String tempRedirectUrl = facebookFactory.getRedirectURL(redirectUrl, Display.POPUP, Permission.EMAIL,
+				Permission.OFFLINE_ACCESS, Permission.READ_STREAM, Permission.PUBLISH_STREAM);
+
+		if (code == null) {
+			System.out.println("Go to this URL and then paste the code that you get!");
+			System.out.println(tempRedirectUrl);
+
+			InputStreamReader converter = new InputStreamReader(System.in);
+			BufferedReader in = new BufferedReader(converter);
+
+			code = in.readLine();
+			System.out.println(code);
+			accessToken = facebookFactory.getOAuthAccessToken(code, redirectUrl);
+		} else {
+			accessToken = new OAuthAccessToken(code);
+		}
+
+		return accessToken;
+	}
+}
